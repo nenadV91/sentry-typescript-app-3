7a02216660c7ef0aff13e53b9e50f2aed3252d53
nenadV91 <nenadwebdevelop@gmail.com>
2021-09-05 20:12:13 +0200

    my initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        My app
+      </header>
+
+      <main>
+        <button>Button</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
